d4f5be45513b928805d3b0f7d471a77c8a69b51d
Nguyenttm0907 <93388452+Nguyenttm0907@users.noreply.github.com>
2023-04-05 13:39:39 +0700

    a little fix

--- pages/Home/index.jsx
+++ pages/Home/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Rating } from '../../components';
 
 const Home = () => {
     return (
         <div>
-            <Rating/>
+            
         </div>
     );
 };
