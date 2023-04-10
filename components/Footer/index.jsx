fb60a8ae99bf0540117e8a3110c817c568d16a24
trinitrobenzene <trannhubuu1412@gmail.com>
2023-04-10 22:03:35 +0700

    fixed footer css

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,45 +1,63 @@
 import React from 'react';
 import './style.scss';
-import {FacebookOutlined,InstagramOutlined,LinkedinOutlined } from '@ant-design/icons';
-import { Row,Col } from 'antd';
-export default function Footer(){
+import {
+    FacebookOutlined,
+    InstagramOutlined,
+    LinkedinOutlined,
+} from '@ant-design/icons';
+
+import { Row, Col } from 'antd';
+
+export default function Footer() {
     return (
         <footer>
-            {/* <Container fluid> */}
-                <Row>
-                    <Col xs={24} md={12} lg={6}>
-                        <h3>Dịch vụ</h3>
-                        <ul>
-                            <li><a href="#">Điều khoản sử dụng</a></li>
-                            <li><a href="#">Chính sách bảo mật thông tin cá nhân</a></li>
-                            <li><a href="#">Chính sách bảo mật thanh toán</a></li>
-                        </ul>
-                    </Col>
-                    <Col xs={24} md={12} lg={6}>
-                        <h3>Hỗ trợ</h3>
-                        <ul>
-                            <li><a href="#">Chính sách vận chuyển</a></li>
-                            <li><a href="#">Phương thức thanh toán và xuất hóa đơn</a></li>
-                            <li><a href="#">Thông tin hữu ích</a></li>
-                        </ul>
-                    </Col>            
-                    <Col xs={24} md={12} lg={6}>
-                        <h3> Liên hệ</h3>
-                        <ul>
-                            <li>Email:bookstore@hcmut.edu.vn</li>
-                            <li>Điện thoại:1800 1801</li>
-                            <li style={{justifyContent:"space-between"}}>
-                                    <FacebookOutlined/>
-                                    <InstagramOutlined/>                        
-                                    <LinkedinOutlined />
-                            </li>
-                        </ul>
-                    </Col>               
-                    <Col xs={24} md={12} lg={6}>
-                        <h3>Logo</h3>
-                    </Col>
-                </Row>
-            {/* </Container> */}
+            <Row className="mw">
+                <Col xs={24} md={12} lg={6}>
+                    <h3>Dịch vụ</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Điều khoản sử dụng</a>
+                        </li>
+                        <li>
+                            <a href="#">Chính sách bảo mật thông tin cá nhân</a>
+                        </li>
+                        <li>
+                            <a href="#">Chính sách bảo mật thanh toán</a>
+                        </li>
+                    </ul>
+                </Col>
+                <Col xs={24} md={12} lg={6}>
+                    <h3>Hỗ trợ</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Chính sách vận chuyển</a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Phương thức thanh toán và xuất hóa đơn
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">Thông tin hữu ích</a>
+                        </li>
+                    </ul>
+                </Col>
+                <Col xs={24} md={12} lg={6}>
+                    <h3> Liên hệ</h3>
+                    <ul>
+                        <li>Email:bookstore@hcmut.edu.vn</li>
+                        <li>Điện thoại:1800 1801</li>
+                        <li style={{ justifyContent: 'space-between' }}>
+                            <FacebookOutlined />
+                            <InstagramOutlined />
+                            <LinkedinOutlined />
+                        </li>
+                    </ul>
+                </Col>
+                <Col xs={24} md={12} lg={6}>
+                    <h3>Logo</h3>
+                </Col>
+            </Row>
         </footer>
     );
-};
+}
