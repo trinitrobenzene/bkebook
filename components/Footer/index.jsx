7597dd05d6487b050021273312682b06ff586af1
Nguyenttm0907 <93388452+Nguyenttm0907@users.noreply.github.com>
2023-04-12 18:16:34 +0700

    resolve conflict

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -11,55 +11,6 @@ import { Row, Col } from 'antd';
 export default function Footer() {
     return (
         <footer>
-<<<<<<< HEAD
-            <Row className="mw">
-                <Col xs={24} md={12} lg={6}>
-                    <h3>Dịch vụ</h3>
-                    <ul>
-                        <li>
-                            <a href="#">Điều khoản sử dụng</a>
-                        </li>
-                        <li>
-                            <a href="#">Chính sách bảo mật thông tin cá nhân</a>
-                        </li>
-                        <li>
-                            <a href="#">Chính sách bảo mật thanh toán</a>
-                        </li>
-                    </ul>
-                </Col>
-                <Col xs={24} md={12} lg={6}>
-                    <h3>Hỗ trợ</h3>
-                    <ul>
-                        <li>
-                            <a href="#">Chính sách vận chuyển</a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Phương thức thanh toán và xuất hóa đơn
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">Thông tin hữu ích</a>
-                        </li>
-                    </ul>
-                </Col>
-                <Col xs={24} md={12} lg={6}>
-                    <h3> Liên hệ</h3>
-                    <ul>
-                        <li>Email:bookstore@hcmut.edu.vn</li>
-                        <li>Điện thoại:1800 1801</li>
-                        <li style={{ justifyContent: 'space-between' }}>
-                            <FacebookOutlined />
-                            <InstagramOutlined />
-                            <LinkedinOutlined />
-                        </li>
-                    </ul>
-                </Col>
-                <Col xs={24} md={12} lg={6}>
-                    <h3>Logo</h3>
-                </Col>
-            </Row>
-=======
             {/* <Container fluid> */}
                 <Row className='mw'>
                     <Col xs={24} md={12} lg={6}>
@@ -95,7 +46,6 @@ export default function Footer() {
                     </Col>
                 </Row>
             {/* </Container> */}
->>>>>>> master
         </footer>
     );
 }
