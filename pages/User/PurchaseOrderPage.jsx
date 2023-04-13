6f7d717cfb53d479fd66a745dfbf2582aa1e1ce7
Nguyenttm0907 <93388452+Nguyenttm0907@users.noreply.github.com>
2023-04-13 14:28:33 +0700

    render data to history page

--- pages/User/PurchaseOrderPage.jsx
+++ pages/User/PurchaseOrderPage.jsx
@@ -1,6 +1,7 @@
 import { Divider } from 'antd';
 import React from 'react'
 import OrderItem from '../../components/History/OrderItem';
+import { useGlobalCtx } from '../../components/GlobalContext';
 
 const PurchaseOrderPage = () => {
   const fakeData = [
@@ -21,31 +22,18 @@ const PurchaseOrderPage = () => {
           count: 2,
           price: 120000,
         },
-
       ]
     },
-    {
-      date: '27/02/2022',
-      status: 'progress',
-      method: 'Zalopay',
-      items: [
-        {
-          name: 'Vật Lý Đại Cương 1',
-          publisher: 'Nhà xuất bản Đại học Quốc gia TP.HCM',
-          count: 1,
-          price: 120000,
-        },
-      ]
-    },
-   
   ]
+  const {globalUser} = useGlobalCtx();
+  console.log(globalUser)
 
 
   return (
     <div className="history">
         <Divider orientation="left">LỊCH SỬ MUA HÀNG</Divider>
         {
-          fakeData.map((item, index) => (
+          globalUser && globalUser.purchase.map((item, index) => (
             <OrderItem order={item} key={index}/>
           ))
         }
