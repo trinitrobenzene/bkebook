5dd1b3cd626edf4f8e995a613f249e4140c96d39
Ái Nguyễn <nguyendieuai082001@gmail.com>
2023-04-20 22:27:16 +0700

    update

--- components/Header/dropdown.jsx
+++ components/Header/dropdown.jsx
@@ -7,16 +7,36 @@ const items = [
         label: 'Sách mới',
         children: [
             {
-                label: 'Giáo trình',
+                label: 'Chính trị – pháp luật',
                 key: '1-1',
               },
               {
-                label: 'Tiểu thuyết',
+                label: 'Khoa học công nghệ – Kinh tế',
                 key: '1-2',
               },
               {
-                label: 'Truyện tranh',
+                label: 'Văn học nghệ thuật',
                 key: '1-3',
+              },
+              {
+                label: 'Văn hóa xã hội – Lịch sử',
+                key: '1-4',
+              },
+              {
+                label: 'Giáo trình',
+                key: '1-5',
+              },
+              {
+                label: 'Truyện, tiểu thuyết',
+                key: '1-6',
+              },
+              {
+                label: 'Tâm lý, tâm linh, tôn giáo',
+                key: '1-7',
+              },
+              {
+                label: 'Thiếu nhi',
+                key: '1-8',
               },
         ],
       },
@@ -26,16 +46,36 @@ const items = [
         label: 'Sách Cũ',
         children: [
             {
+                label: 'Chính trị – pháp luật',
+                key: '2-1',
+              },
+              {
+                label: 'Khoa học công nghệ – Kinh tế',
+                key: '2-2',
+              },
+              {
+                label: 'Văn học nghệ thuật',
+                key: '2-3',
+              },
+              {
+                label: 'Văn hóa xã hội – Lịch sử',
+                key: '2-4',
+              },
+              {
                 label: 'Giáo trình',
-                key: '1-1',
+                key: '2-5',
               },
               {
-                label: 'Tiểu thuyết',
-                key: '1-2',
+                label: 'Truyện, tiểu thuyết',
+                key: '2-6',
               },
               {
-                label: 'Truyện tranh',
-                key: '1-3',
+                label: 'Tâm lý, tâm linh, tôn giáo',
+                key: '2-7',
+              },
+              {
+                label: 'Thiếu nhi',
+                key: '2-8',
               },
         ],
       },
